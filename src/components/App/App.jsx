7f09174ce57ef4b9a905e597185f5ad2afabd236
Rastim Safarov <rast875@gmail.com>
2023-11-17 03:54:30 +0000

    feat: Add App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-// import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 
 import Main from '../pages/Main/Main';
 import Movies from '../pages/Movies/Movies';
@@ -16,8 +16,8 @@ function App() {
   return (
     // <CurrentUserContext.Provider value={currentUser} >
       <div className="app">
-        {/* <Routes>
-          <Route path="/" element={<AboutProject />} />
+        <Routes>
+          <Route path="/" element={<Main />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/saved-movies" element={<SavedMovies />} />
           <Route path="/profile" element={<Profile />} />
@@ -25,15 +25,8 @@ function App() {
           <Route path="/signin" element={<Login />} />
           <Route path="/signup" element={<Register />} />
           <Route path="*" element={<NotFound404 />} />
-        </Routes> */}
+        </Routes>
 
-        {/* <Main /> */}
-        {/* <Movies /> */}
-        {/* <SavedMovies /> */}
-        {/* <Profile /> */}
-        {/* <Register /> */}
-        {/* <Login /> */}
-        <NotFound404 />
       </div>
     // </CurrentUserContext.Provider>
   );
